782ab21a3355f23842f366fcdf9a219c1f8cb5ce
Cody I <cody@codyi.net>
2020-07-10 17:08:09 -0700

    Added CircleCI config.yml

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/CGAPI';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/CGAPI_test';
-exports.PORT = process.env.PORT || 3000;
+exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/CGAPI";
+exports.TEST_DATABASE_URL =
+  process.env.TEST_DATABASE_URL || "mongodb://localhost/CGAPI_test";
+exports.PORT = process.env.PORT || 3001;
